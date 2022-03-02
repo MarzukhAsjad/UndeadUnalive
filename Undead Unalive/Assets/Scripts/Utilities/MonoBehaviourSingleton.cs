8d2b4351e61e2964e5427356997ebce37fead638
EMCJava <loyusum246807952@gmail.com>
2022-03-02 23:26:18 +0800

    Added: basic First Person controller & a sample model animation

---
+++ Undead Unalive/Assets/Scripts/Utilities/MonoBehaviourSingleton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Utilities
+{
+    public class MonoBehaviourSingleton<T> : MonoBehaviour where T : class
+    {
+        private static MonoBehaviourSingleton<T> _instance;
+        public static T Instance => _instance as T;
+
+        private void Awake()
+        {
+            // If there is an instance, and it's not me, delete myself.
+ 
+            if (_instance != null && !ReferenceEquals(_instance, this))
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                _instance = this;
+            }
+        }
+    }
+}
