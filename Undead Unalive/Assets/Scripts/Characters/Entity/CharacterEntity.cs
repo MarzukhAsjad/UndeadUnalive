8e12c4ef6327aa1e1bb35443ff01a5811e6c67f6
EMCJava <loyusum246807952@gmail.com>
2022-03-21 18:16:25 +0800

    Added: CharacterEntity for basic health system

---
+++ Undead Unalive/Assets/Scripts/Characters/Entity/CharacterEntity.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Characters.Entity
+{
+    public class CharacterEntity : MonoBehaviour
+    {
+        private float _health = 0;
+
+        public void ChangeHealth(float newHealth)
+        {
+            _health = newHealth;
+        }
+
+        public void AddDeltaHealth(float deltaHealth)
+        {
+            _health += deltaHealth;
+        }
+    }
+}
