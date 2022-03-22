234ad615c040ebe16d0eed621205582cf1719513
EMCJava <loyusum246807952@gmail.com>
2022-03-22 21:29:14 +0800

    Added: essential files

---
+++ Undead Unalive/Assets/Scripts/Characters/Controller/EnemyController.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Characters.Controller
+{
+    public class EnemyController : MonoBehaviour
+    {
+        
+    }
+}
---
+++ Undead Unalive/Assets/Scripts/Characters/Controller/MobController.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Characters.Controller
+{
+    public class MobController : MonoBehaviour
+    {
+        
+    }
+}
