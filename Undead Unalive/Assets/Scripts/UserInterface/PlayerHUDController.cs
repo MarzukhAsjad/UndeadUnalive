b52779534c50c579de41ff1b0aef7870675342b2
EMCJava <loyusum246807952@gmail.com>
2022-04-06 10:29:34 +0800

    Added enemy indicator

--- Undead Unalive/Assets/Scripts/UserInterface/PlayerHUDController.cs
+++ Undead Unalive/Assets/Scripts/UserInterface/PlayerHUDController.cs
@@ -1,11 +1,14 @@
+using System;
 using Characters.Entity;
 using Interface.Controller;
 using UnityEngine;
 using UnityEngine.Events;
 using Utilities;
+using Random = UnityEngine.Random;
 
 namespace Interface
 {
+    using EnemyType = GameObject;
     public class PlayerHUDController : MonoBehaviourSingleton<PlayerHUDController>
     {
         /*
@@ -15,12 +18,22 @@ namespace Interface
          */
         [SerializeField] private GameObject gameOverScreen;
 
+        private Camera activeCamera;
         private GameObject _playerHUDObject;
         private GameObject _healthBar;
 
         private CharacterEntity _playerEntity;
 
         private readonly UnityEvent<float, float> _healthChangeProxy = new();
+
+        public GameObject enemyIndicatorPrefab;
+
+        /*
+         *
+         * Enemy indicator
+         * 
+         */
+        private TimeoutList _enemyIndicatorList;
 
         private void OnEnable()
         {
@@ -29,14 +42,141 @@ namespace Interface
 
             _playerEntity = GameObject.FindWithTag("Player")?.GetComponent<CharacterEntity>();
             Debug.Assert(_playerEntity != null);
-            
+
             _healthBar = _playerHUDObject.transform.Find("HealthBar").gameObject;
-            
+
             _playerEntity.onHealthChanged.AddListener(() => _healthChangeProxy.Invoke(_playerEntity.GetHealth(), _playerEntity.GetDefaultMaxHealth()));
             var barController = _healthBar.AddComponent<BarController>();
             barController.changeEvent = _healthChangeProxy;
             barController.defaultValue = _playerEntity.GetHealth();
             barController.defaultMax = _playerEntity.GetDefaultMaxHealth();
+        }
+
+        private void Start()
+        {
+            activeCamera = Camera.main;
+            _enemyIndicatorList = gameObject.AddComponent<TimeoutList>();
+        }
+
+        private float timer = 0;
+
+        private void Update()
+        {
+            timer += Time.deltaTime;
+            if (timer > 0.5f)
+            {
+                var newObj = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+                var ranPos = Random.insideUnitCircle * 10;
+                newObj.transform.position = new Vector3(ranPos.x, 1, ranPos.y);
+                newObj.AddComponent<Rigidbody>();
+
+                EnemyNotifyPlayer(newObj);
+
+                timer = 0;
+            }
+
+            _enemyIndicatorList.ForEach((k, d) =>
+            {
+                FindScreenEdgeLocationForWorldLocation(((GameObject)k).transform.position, 0.9f, out var outScreenPosition, out var outRotationAngleDegrees, out var bIsOnScreen);
+
+                ((RectTransform)d).gameObject.SetActive(!bIsOnScreen);
+                
+                ((RectTransform)d).anchoredPosition = outScreenPosition;
+                ((RectTransform)d).rotation = Quaternion.Euler(0, 0, outRotationAngleDegrees - 90);
+            });
+        }
+
+        private void FindScreenEdgeLocationForWorldLocation(Vector3 inLocation, float edgePercent, out Vector2 outScreenPosition, out float outRotationAngleDegrees, out bool bIsOnScreen)
+        {
+            // inspired from https://forums.unrealengine.com/t/easy-offscreen-indicator-blueprint-node/21342?filter=summary
+
+            bIsOnScreen = false;
+            outRotationAngleDegrees = 0f;
+            Vector2 screenPosition;
+
+            var pixelRect = activeCamera.pixelRect;
+            var viewportSize = pixelRect.size;
+            var viewportCenter = pixelRect.center;
+
+            var forward = activeCamera.transform.forward;
+            var offset = (inLocation - _playerEntity.transform.position).normalized;
+
+            var dotProduct = Vector3.Dot(forward, offset);
+            var bLocationIsBehindCamera = dotProduct < 0;
+
+            if (bLocationIsBehindCamera)
+            {
+                // For behind the camera situation, we cheat a little to put the
+                // marker at the bottom of the screen so that it moves smoothly
+                // as you turn around. Could stand some refinement, but results
+                // are decent enough for most purposes.
+
+                var position = _playerEntity.transform.position;
+
+                var diffVector = inLocation - position;
+                var Inverted = diffVector * -1f;
+                var newInLocation = position + Inverted;
+
+                screenPosition = activeCamera.WorldToScreenPoint(newInLocation);
+                screenPosition.x = viewportSize.x - screenPosition.x;
+                screenPosition.y = viewportSize.y - screenPosition.y;
+            }
+            else
+            {
+                screenPosition = activeCamera.WorldToScreenPoint(inLocation);
+            }
+
+            // Check to see if it's on screen. If it is, ProjectWorldLocationToScreen is all we need, return it.
+            if (screenPosition.x >= 0f && screenPosition.x <= viewportSize.x
+                                       && screenPosition.y >= 0f && screenPosition.y <= viewportSize.y && !bLocationIsBehindCamera)
+            {
+                outScreenPosition = screenPosition;
+                bIsOnScreen = true;
+                return;
+            }
+
+            screenPosition -= viewportCenter;
+
+            var angleRadians = Mathf.Atan2(screenPosition.y, screenPosition.x);
+            angleRadians -= Mathf.Deg2Rad * 90;
+
+            outRotationAngleDegrees = Mathf.Rad2Deg * angleRadians + 180f;
+
+            var cos = Mathf.Cos(angleRadians);
+            var sin = -Mathf.Sin(angleRadians);
+
+            var m = cos / sin;
+
+            var screenBounds = viewportCenter * edgePercent;
+
+            screenPosition = cos > 0 ? new Vector2(screenBounds.y / m, screenBounds.y) : new Vector2(-screenBounds.y / m, -screenBounds.y);
+
+            if (screenPosition.x > screenBounds.x)
+            {
+                screenPosition = new Vector2(screenBounds.x, screenBounds.x * m);
+            }
+            else if (screenPosition.x < -screenBounds.x)
+            {
+                screenPosition = new Vector2(-screenBounds.x, -screenBounds.x * m);
+            }
+
+            screenPosition += viewportCenter;
+            outScreenPosition = screenPosition;
+        }
+
+        public void EnemyNotifyPlayer(EnemyType enemy)
+        {
+            RectTransform imageRect = null;
+            Action destructor = null;
+
+            if (!_enemyIndicatorList.HasElement(enemy))
+            {
+                imageRect = Instantiate(enemyIndicatorPrefab).GetComponent<RectTransform>();
+                imageRect.transform.SetParent(_playerHUDObject.transform);
+                destructor = () => { Destroy(imageRect.gameObject); };
+            }
+
+            _enemyIndicatorList.RenewElements(enemy, imageRect, 2, destructor);
         }
 
         public void DoGameOverScreen()
---
+++ Undead Unalive/Assets/Scripts/Utilities/TimeoutList.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Object = System.Object;
+
+namespace Utilities
+{
+    public class TimeoutList : MonoBehaviour
+    {
+        private class ListElement
+        {
+            public object Key;
+            public object Data;
+            public Action Destructor;
+            public float StartTime;
+            public float Duration;
+            public float EndTime;
+        }
+
+        private readonly LinkedList<ListElement> _elements = new();
+        private readonly Dictionary<object, ListElement> _elementMap = new();
+
+        private void Update()
+        {
+            var currentTime = Time.fixedTime;
+            while (_elements.Count > 0 && _elements.First().EndTime < currentTime)
+            {
+                var targetElement = _elements.First();
+                targetElement.Destructor?.Invoke();
+                _elementMap.Remove(targetElement.Key);
+                _elements.RemoveFirst();
+            }
+        }
+
+        private void InsertValueToList(ListElement element)
+        {
+            var insertAfter = _elements.LastOrDefault(e => e.EndTime < element.EndTime);
+            if (insertAfter is null)
+            {
+                _elements.AddFirst(element);
+            }
+            else
+            {
+                _elements.AddAfter(_elements.Find(insertAfter), element);
+            }
+        }
+
+        public bool HasElement(object key)
+        {
+            return _elementMap.ContainsKey(key);
+        }
+        
+        public void RenewElements(object key, object data, float timeout = 1, Action destructor = null)
+        {
+            var currentTime = Time.fixedTime;
+
+            if (_elementMap.TryGetValue(key, out var element))
+            {
+                element.Data = data ?? element.Data;
+                element.Destructor = destructor ?? element.Destructor;
+                element.StartTime = currentTime;
+                element.EndTime = currentTime + timeout;
+                element.Duration = timeout;
+
+                if (_elements.Count > 1)
+                {
+                    _elements.Remove(element);
+                    InsertValueToList(element);
+                }
+            }
+            else
+            {
+                var newValue = new ListElement
+                {
+                    Key = key,
+                    Data = data,
+                    Destructor = destructor,
+                    StartTime = currentTime,
+                    EndTime = currentTime + timeout,
+                    Duration = timeout
+                };
+
+                InsertValueToList(newValue);
+                _elementMap.Add(key, newValue);
+            }
+        }
+
+        public void ForEach(Action<object, object> func)
+        {
+            foreach (var listElement in _elements) func(listElement.Key, listElement.Data);
+        }
+    }
+}
