51719816405ad4df39440c762bb4199941c26336
Aryan Agrawal <80872992+Codes-of-Aryan@users.noreply.github.com>
2022-04-22 18:25:12 +0530

    mob stays behind you
    The mob will always stay behind the player, this however also means seeing the mob would be difficult (if you rotate they will rotate)

--- Undead Unalive/Assets/Scripts/Characters/Controller/MobController.cs
+++ Undead Unalive/Assets/Scripts/Characters/Controller/MobController.cs
@@ -11,27 +11,32 @@ namespace Characters.Controller
         public GameObject safeHouse;
         public GameObject zombie;
         public int shieldRadius;
+        public Vector3 offset; 
 
-
+        private Camera mainCamera;
         private GameObject player;
         private Rigidbody rb;
         private bool enable;
         private float distance;
         private Animator animator;
         private Vector3 pos = new Vector3(204.29f, 0, 296.37f);
+        private Vector3 desiredPosition; 
         
         public void Start()
         {
             safeHouse = new GameObject();
-            safeHouse.transform.position = pos; 
+            safeHouse.transform.position = pos;
+            
 
             agent = this.GetComponent<NavMeshAgent>();
             rb = this.GetComponent<Rigidbody>();
             animator = this.GetComponent<Animator>();
-            player = GameObject.FindGameObjectWithTag("Player"); 
+            player = GameObject.FindGameObjectWithTag("Player");
+            mainCamera = GameObject.Find("Main Camera").GetComponent<Camera>(); 
 
             enable = true;
-            shieldRadius = 4; 
+            shieldRadius = 0;
+            offset = new Vector3(0, 0, 5); 
 
         }
 
@@ -55,6 +60,9 @@ namespace Characters.Controller
 
             }*/
 
+            //rotating the offset
+            offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X"), Vector3.up) * offset; 
+
              //if disabled, mob should just stay at there current position
             if (!enable)
             {
@@ -67,11 +75,12 @@ namespace Characters.Controller
             else //mob should follow player
             { 
 
-                distance = Vector3.Distance(player.transform.position, this.transform.position); 
+                distance = Vector3.Distance(player.transform.position, this.transform.position);
+                desiredPosition = player.transform.position + offset; 
                 
                 if (distance > shieldRadius) {
-                   
-                    agent.SetDestination(player.transform.position);
+
+                    agent.SetDestination(desiredPosition);//player.transform.position);
                     Run(); 
 
                 } else { //if distance is less than the shield radius, stop
