925a43fb7612ba103f8ebabec192adcbf9bbfb20
Smaran Goli <u3583070@connect.hku.hk>
2022-04-29 10:43:54 +0530

    Revert "AddedZombieZones12&13"
    This reverts commit ef9857d16d87a88846e8f0914ebb202d91e86b84.

--- Undead Unalive/Assets/Scripts/Weapon/PickUpGrenade.cs
+++ Undead Unalive/Assets/Scripts/Weapon/PickUpGrenade.cs
@@ -4,7 +4,8 @@ using Managers;
 using UnityEngine;
 
 public class PickUpGrenade : MonoBehaviour
-{   
+{
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
