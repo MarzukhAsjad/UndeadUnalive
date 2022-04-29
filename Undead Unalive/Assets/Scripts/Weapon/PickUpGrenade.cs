ef9857d16d87a88846e8f0914ebb202d91e86b84
Smaran Goli <u3583070@connect.hku.hk>
2022-04-29 10:40:00 +0530

    AddedZombieZones12&13

--- Undead Unalive/Assets/Scripts/Weapon/PickUpGrenade.cs
+++ Undead Unalive/Assets/Scripts/Weapon/PickUpGrenade.cs
@@ -4,8 +4,7 @@ using Managers;
 using UnityEngine;
 
 public class PickUpGrenade : MonoBehaviour
-{
-    
+{   
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
